27f7b21b9916e0cb075584bffab4a641fcb69047
magnetar-star <matthew.lvk@gmail.com>
2025-01-18 02:47:55 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod randn_generator;
 
-use randn_generator::gaussian_generator;
-
+use randn_generator::{gaussian_generator, 
+                        gaussian_generator_array, 
+                        NumberType};
+use rand::distributions::uniform::SampleUniform;
+use rand_distr::{Distribution, Normal};
+use rand::{distributions::Uniform, Rng};
 
 fn main() {
     println!("Start");
     let n: f64 = gaussian_generator(2.0, 5.0);
-    println!("random number is: {0}", n);
-    println!("End");
+    let r_array = gaussian_generator_array(
+                                    2.0, 5.0, 10000000);
+    println!("random normal number is: {0}", n);
+    println!("random normal array is: {0}", r_array);
 }
--- src/randn_generator.rs
+++ src/randn_generator.rs
@@ -1,9 +1,21 @@
 use ndarray::prelude::*;
-use ndarray::{Array, Array2};
-use rand::prelude::*;
-use rand::distributions::uniform::SampleUniform;
+// use ndarray::{Array, Array2};
+// use rand::prelude::*;
+use rand::distributions::{uniform::SampleUniform, Uniform};
+use rand::Rng;
 use rand_distr::{Distribution, Normal};
 use num::Float;
+
+
+/// Represents either a u32 or u64 number
+#[derive(Debug)]
+pub enum NumberType {
+    U32(u32),
+    U64(u64),
+    I32(i32),
+    I64(i64),
+}
+
 
 /// Generates a random number from a Gaussian distribution with the given mean and standard deviation.
 ///
@@ -25,8 +37,29 @@ where
     return T::from(v).unwrap();
 }
 
-fn gaussian_generator_array(mean: f64, std_dev: f64) -> f64 {
-    let normal: Normal<f64> = Normal::new(mean, std_dev).unwrap();
-    let v: f64 = normal.sample(&mut rand::thread_rng());
-    return v;
-}
+
+/// Generates a random number from a Gaussian distribution with the given mean and standard deviation.
+///
+/// # Arguments
+///
+/// * `mean` - The mean of the Gaussian distribution.
+/// * `std_dev` - The standard deviation of the Gaussian distribution.
+///
+/// # Returns
+///
+/// A random number sampled from the specified Gaussian distribution.
+pub fn gaussian_generator_array<T>(mean: T, std_dev: T, size: usize) 
+-> Array1<T>
+where
+    T: Float + SampleUniform
+    {
+        let normal = Normal::new(mean.to_f64().unwrap(),
+                                    std_dev.to_f64().unwrap()).unwrap();
+        let mut rng = rand::thread_rng();
+        let mut array = Array1::<T>::zeros(size);
+        for i in 0..size {
+            let v: f64 = normal.sample(&mut rng);
+            array[i] = T::from(v).unwrap();
+        }
+        return array;
+    }
